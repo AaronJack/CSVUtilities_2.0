a62f5dca55456906aadc24dec54acfa251aca27b
BT_1N3_09@bktech.local <BT_1N3_09@bktech.local>
2018-12-13 11:36:51 -0500

    Initial commit

---
+++ src/com/company/CSVUtilities.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class CSVUtilities {
+    private ArrayList CSVData = new ArrayList();
+    private int numColumns = 0;
+
+    public CSVUtilities(File csv){
+
+    }
+}
